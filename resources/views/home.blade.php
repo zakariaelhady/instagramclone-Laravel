d5c68c4c67088bbd51ba3df77c655c622890bbaf
zakaria <zakariaelhady2017@gmail.com>
2022-02-10 19:32:48 +0100

    adding profile (models,controllers,migration)

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,21 +4,21 @@
 <div class="container mx-auto col-8">
     <div class="row mx-auto">
         <div class="col-3 px-5 pt-3 mx-2">
-            <img src="img/post1.jpeg " class="rounded-circle" style="width: 155px; height: 155px;">
+            <img src="/img/post1.jpeg " class="rounded-circle" style="width: 155px; height: 155px;">
         </div>
         <div class="col-8 pt-3">
             <div>
-                <h2>Zakariaelhady</h2>
+                <h2>{{$user->username}}</h2>
             </div>
             <div class="row">
                 <div class="col-2"><strong class="p-1">23</strong>posts</div>
                 <div class="col-2"><strong class="p-1">203</strong>followers</div>
                 <div class="col-2"><strong class="p-1">63</strong>following</div>
             </div>
-            <div class="pt-4 fs-6"><strong>zakaria el hadi</strong> </div>
+            <div class="pt-4 fs-6"><strong>{{$user->name}}</strong> </div>
             <div class="row">
-                <div class=" text-muted fs-5 " >Photographie et vidéographie</div>
-                <div class="fw-normal fs-6">ENSAK Kenitra 🇲🇦 <br>Génie informatique</div>               
+                <div class=" text-muted fs-5 " >{{$user->profile->category ?? 'N/A'}}</div>
+                <div class="fw-normal fs-6">{{$user->profile->desc ?? 'N/A'}}</div>               
             </div>
             <div class="row">
                 <a class="text-decoration-none text-primary " href="https://www.instagram.com/zakariaelhady">www.instagram.com/zakariaelhady</a>
@@ -28,16 +28,16 @@
     <div class="row pt-4 "> <hr></div>
     <div class="row ">
         <div class="col-4 pb-1">
-            <img src="img/post2.png" class="rounded" style="width:300px;" href="#">
+            <img src="/img/post2.png" class="rounded" style="width:300px;" href="#">
         </div>
         <div class="col-4 pb-1">
-            <img src="img/profile2.png" class="rounded" style="width:300px;" href="#">
+            <img src="/img/profile2.png" class="rounded" style="width:300px;" href="#">
         </div>
         <div class="col-4 pb-1">
-            <img src="img/post2.png" class="rounded" style="width:300px;" href="#">
+            <img src="/img/post2.png" class="rounded" style="width:300px;" href="#">
         </div>
         <div class="col-4 pb-1">
-            <img src="img/profile2.png" class="rounded" style="width:300px;" href="#">
+            <img src="/img/profile2.png" class="rounded" style="width:300px;" href="#">
         </div>
         
     </div>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -24,7 +24,7 @@
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container mx-auto col-8">
                 <a class="navbar-brand d-flex " href="{{ url('/') }}">
-                    <div class="container mr-5"><img src="img/tag.png" alt="logo"  style="height:30px; "></div>
+                    <div class="container mr-5"><img src="/img/tag.png" alt="logo"  style="height:30px; "></div>
                     <div class="container pl-5 pt-1" style=" border-left: 2px solid gray;">Megagram</div> 
                 </a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
@@ -55,7 +55,7 @@
                         @else
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }}
+                                    {{ Auth::user()->username }}
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
