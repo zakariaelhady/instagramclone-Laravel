1eb3ae20ccfa9b71813f34be1cbee04e6688d210
zakaria <zakariaelhady2017@gmail.com>
2022-02-11 17:14:42 +0100

    modify post show

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -7,12 +7,30 @@
         <div class="col-lg-8">
             <img src="/storage/{{$post->img}}" class="w-100" alt="">
         </div>
-        <div class="col">
-            <div class="row">
-                {{$post->user->username}}
+        <div class="col-lg-4">
+            <div class="row align-items-center">
+                <div class="col-lg-2 px-1 pt-1 mx-2">
+                    <img src="/storage/{{$post->user->profile->img}}" class="rounded-circle" style="width: 50px; height: 50px;">
+                </div>
+                <div class="col-lg-2 px-1 pl-3">
+                    <p class="col-lg-2 fw-bold fs-6">
+                        <a href="/profile/{{$post->user->id}}" class="text-decoration-none text-dark">{{$post->user->username}}</a>
+                    </p>
+                </div>
+                <div class="col-lg-1 px-1 pl-3 mx-5">
+                    <p class="col-lg-2 fw-bold fs-6">
+                        <a href="#" class="text-decoration-none text-info">Follow</a>
+                    </p>
+                </div>
             </div>
-            <div class="row">
-                {{$post->caption}}
+            <hr>
+            <div class=" d-flex">
+                <span class="pr-5 mx-1">
+                    <a href="/profile/{{$post->user->id}}" class="text-decoration-none fw-bold text-dark">{{$post->user->username}}</a>
+                </span>
+                <span class="wrap ">
+                    {{$post->caption}}
+                </span>
             </div>
         </div>
     </div>
